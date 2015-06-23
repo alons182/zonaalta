1647a6bf881bf4dd512a6ff3dbb33bba55b8e409
Alonso Chavarria <alons182@hotmail.com>
2015-06-23 15:55:13 -0600

    se creo custom post type anuncios

--- wp-content/themes/zonaalta/functions.php
+++ wp-content/themes/zonaalta/functions.php
@@ -33,3 +33,96 @@ function site_favicon() {
 }
 
 add_action('wp_head', 'site_favicon');
+
+add_filter( 'rwmb_meta_boxes', 'zonaalta_register_meta_boxes' );
+
+function zonaalta_register_meta_boxes( $meta_boxes )
+{
+
+    $prefix = 'rw_';
+
+    // 1st meta box
+    $meta_boxes[] = array(
+        'id'       => 'ads_content',
+        'title'    => 'Anuncios',
+        'pages'    => array( 'post', 'page', 'ads' ),
+        'context'  => 'normal',
+        'priority' => 'high',
+
+        'fields' => array(
+            
+             array(
+                'name'  => 'Image',
+                'desc'  => 'Format: Image File',
+                'id'    => $prefix . 'ad_image',
+                'type'  => 'image',
+                'std'   => '',
+                'class' => 'custom-class'
+                
+            ),
+              array(
+                'name'  => 'Url',
+                'desc'  => 'Format: Url',
+                'id'    => $prefix . 'ad_url',
+                'type'  => 'url',
+                'std'   => '',
+                'class' => 'custom-class'
+                
+            )
+
+        )
+    );
+    
+
+
+    return $meta_boxes;
+}
+
+//custom post type services
+function my_custom_post_ads() {
+  $labels = array(
+    'name'               => _x( 'Anuncios', 'post type general name' ),
+    'singular_name'      => _x( 'Anuncio', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', 'book' ),
+    'add_new_item'       => __( 'Add New Anuncio' ),
+    'edit_item'          => __( 'Edit Anuncio' ),
+    'new_item'           => __( 'New Anuncio' ),
+    'all_items'          => __( 'All ads' ),
+    'view_item'          => __( 'View Anuncio' ),
+    'search_items'       => __( 'Search Anuncios' ),
+    'not_found'          => __( 'No anuncios found' ),
+    'not_found_in_trash' => __( 'No Anuncios found in the Trash' ), 
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Anuncios'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'description'   => 'Holds our Ads and Ad specific data',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'ads', $args ); 
+}
+add_action( 'init', 'my_custom_post_ads' );
+
+function my_updated_messages( $messages ) {
+  global $post, $post_ID;
+  $messages['ads'] = array(
+    0 => '', 
+    1 => sprintf( __('Anuncio updated. <a href="%s">View Anuncio</a>'), esc_url( get_permalink($post_ID) ) ),
+    2 => __('Custom field updated.'),
+    3 => __('Custom field deleted.'),
+    4 => __('Anuncio updated.'),
+    5 => isset($_GET['revision']) ? sprintf( __('Project restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+    6 => sprintf( __('Anuncio published. <a href="%s">View Anuncio</a>'), esc_url( get_permalink($post_ID) ) ),
+    7 => __('Anuncio saved.'),
+    8 => sprintf( __('Anuncio submitted. <a target="_blank" href="%s">Preview Anuncio</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    9 => sprintf( __('Anuncio scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Anuncio</a>'), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+    10 => sprintf( __('Anuncio draft updated. <a target="_blank" href="%s">Preview Anuncio</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+  );
+  return $messages;
+}
+add_filter( 'post_updated_messages', 'my_updated_messages' );
+
--- wp-content/themes/zonaalta/templates/ads.php
+++ wp-content/themes/zonaalta/templates/ads.php
@@ -1,9 +1,53 @@
+ 
+
  <div class="main__ads">
         <div class="cycle-slideshow"  data-cycle-timeout="4000"  data-cycle-slides=".main__ads__slide">
-        
-	        <div class="main__ads__slide">
-	        	<img src="<?php echo get_template_directory_uri(); ?>/img/banner-anuncios.gif" />
-	        </div>
+        	
+			<?php
+			    $args = array(
+			      'post_type' => 'ads',
+			      			      
+			    );
+			    $projects = new WP_Query( $args );
+			    if( $projects->have_posts() ) {
+			      while( $projects->have_posts() ) {
+			        $projects->the_post();
+					
+					$images = rwmb_meta( 'rw_ad_image', 'type=image&size=full' );
+					$url = rwmb_meta( 'rw_ad_url');
+			        ?>
+			           
+			             <div class="main__ads__slide">
+				        	<a href="<?php echo $url; ?>" target="_blank" title="<?php the_title(); ?>">
+				        	<?php 								
+								
+								if ( $images ) : ?>
+									<?php 
+											
+
+											foreach ( $images as $image )
+											{
+											    
+											    echo "<img src=".$image['url']." alt='Anuncio' />";
+											}
+											
+										?>
+				        	
+								<?php endif; ?>
+				        	</a>
+				        </div>
+
+
+			          
+			        <?php
+			      }
+			    }
+			    else {
+			      echo 'No projects!';
+			    }
+			  ?>
+
+	       
        </div>
        
 
