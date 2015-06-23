a99aed0f8420562bcd017565ce4adb9b0d7eabb9
Alonso Chavarria <alons182@hotmail.com>
2015-06-23 13:54:30 -0600

    cambio twicker

--- wp-content/themes/zonaalta/templates/twicker.php
+++ wp-content/themes/zonaalta/templates/twicker.php
@@ -1,3 +1,3 @@
 <div class="twicker">
-	<script type="text/javascript">(function(){var ticker=document.createElement('script');ticker.type='text/javascript';ticker.async=true;ticker.src='//twitcker.com/ticker/radiozonaalta.js';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);})();</script>
+	<script type="text/javascript">(function(){var ticker=document.createElement('script');ticker.type='text/javascript';ticker.async=true;ticker.src='//twitcker.com/ticker/radiozonaalta.js?speed=4&language=es';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);})();</script>
 </div>
