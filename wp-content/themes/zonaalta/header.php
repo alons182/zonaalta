72e47d8fb87fb29b39e508d7d1209b58834122ba
Alonso Chavarria <alons182@hotmail.com>
2015-07-21 09:25:01 -0600

    fix facebok

--- wp-content/themes/zonaalta/header.php
+++ wp-content/themes/zonaalta/header.php
@@ -20,6 +20,7 @@
             <a href="<?php echo home_url( '/' ); ?>" class="header__logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Radio Zona Alta"></a>
             <div class="header__contact">
                 <span><i class="icon-whatsapp"></i> (506) 8998-1098</span>
+                 <a href="https://www.facebook.com/radiozonaalta" class="header__contact__fb" target="_blank"><i class="icon-facebook"></i> Facebook</a>
             </div>
             
             <button id="btn-menu" class="header__btn-menu"><i class="icon-menu"></i></button>
