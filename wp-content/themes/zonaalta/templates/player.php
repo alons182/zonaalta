4cbaff50682dd789c30a7afc89c9f79dab996488
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-27 09:23:08 -0600

    se cambio player zona alta

--- wp-content/themes/zonaalta/templates/player.php
+++ wp-content/themes/zonaalta/templates/player.php
@@ -1,9 +1,9 @@
 <div class="main__player">
-	<iframe src="http://generadoraac.com/miniv1.php?ip=192.99.38.200&puerto=9990/stream" frameborder="0"  width="270px" height="270px"> </iframe>
-    
-    <div class="main__player__container">
+	<!-- <iframe src="http://generadoraac.com/miniv1.php?ip=192.99.38.200&puerto=9990/stream" frameborder="0"  width="270px" height="270px"> </iframe> -->
+    <iframe src="http://generadoraac.com/reprov1.php?ip=servicompcr.net&puerto=9990/stream" frameborder="0"  width="340px" height="70px" style="margin-left: 2.5rem;"> </iframe>
+    <!--<div class="main__player__container">
     	<img src="<?php echo get_template_directory_uri(); ?>/img/play.png" alt="Inicio" id="sm-button"/>
-    </div>
+    </div>-->
     <div class="main__tunein">
        Escúchenos desde <a href="http://tunein.com/radio/Radio-Zona-Alta-s197588/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/tunein.png" alt="Tune In Radio"></a><br />
        <a href="http://www.radios.co.cr/region/san-jose-am-y-digital/#radio-zona-alta-monteverde" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/radios-online.png" alt="Radios Online"></a>
