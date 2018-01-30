594978de922a667a6c2b7e444e87977793803f78
Alonso Avotz <alonso@avotz.com>
2018-01-30 10:14:26 -0600

    new player

--- wp-content/themes/zonaalta/templates/player.php
+++ wp-content/themes/zonaalta/templates/player.php
@@ -1,5 +1,8 @@
 <div class="main__player">
-    <!-- <object type="text/html" data="http://generadoraac.com/live/lite.js?live=4523" style="width: auto; height: auto;"> -->
+    <div class="player">
+        <object type="text/html" data="http://generadoraac.com/live/lite.js?live=4523" style="width: 300px; height: 200px;">
+    </div>
+    
     <div class="main__tunein">
        Escúchenos desde <a href="http://tunein.com/radio/Radio-Zona-Alta-s197588/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/tunein.png" alt="Tune In Radio"></a><br />
        <a href="http://www.radios.co.cr/region/san-jose-am-y-digital/#radio-zona-alta-monteverde" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/radios-online.png" alt="Radios Online"></a>
