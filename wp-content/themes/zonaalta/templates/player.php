fd20fb283155a2196a143d7f89790b82d0b6f4c1
Alonso Avotz <alonso@avotz.com>
2018-01-30 10:22:57 -0600

    test player

--- wp-content/themes/zonaalta/templates/player.php
+++ wp-content/themes/zonaalta/templates/player.php
@@ -1,7 +1,7 @@
 <div class="main__player">
-    <!-- <div class="player">
-        <object type="text/html" data="http://generadoraac.com/live/lite.js?live=4523" style="width: 300px; height: 200px;" />
-    </div> -->
+    <div class="player">
+        <object type="text/html" data="http://generadoraac.com/live/lite.js?live=4523" style="width: 300px; height: 80px;" />
+    </div>
     
     <div class="main__tunein">
        Escúchenos desde <a href="http://tunein.com/radio/Radio-Zona-Alta-s197588/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/tunein.png" alt="Tune In Radio"></a><br />
